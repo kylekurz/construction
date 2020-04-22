5cf4994a3f795776692417e4183cb130e10ffa9c
Kyle Kurz <kyle@doublekaudio.com>
2020-04-21 21:35:31 -0700

    Set up construction page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+  grid: {
+    display: 'flex',
+    padding: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+  cardHeader: {
+    backgroundColor: theme.palette.grey[200],
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card className={classes.paper} raised>
+          <CardHeader
+            title='Under Construction'
+            titleTypographyProps={{ align: 'center' }}
+            subheaderTypographyProps={{ align: 'center' }}
+            className={classes.cardHeader}
+            />
+          <CardContent>
+              <Typography variant="subtitle1" align="center">
+              This site is currently under construction. Please check back soon.
+              </Typography>
+          </CardContent>
+        </Card>
       </header>
     </div>
   );
